444df8e8a5739cff21c476286d96253ded61961e
hgarner-lab <haley.garner@mccann.com>
2026-06-29 16:52:39 +0100

    Update Pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? "/BRIEF" : undefined,
+  assetPrefix: isGithubPages ? "/BRIEF/" : undefined,
 };
 
 export default nextConfig;
